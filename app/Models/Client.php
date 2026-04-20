e76747350f7ea61cabedb9218680c2a785f07151
daniv04 <danovillalobos0408@gmail.com>
2026-04-20 11:55:56 -0600

    and relationship on client model

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Client extends Model
 {
@@ -15,10 +16,10 @@ class Client extends Model
         'economic_activity_description',
         'email',
         'phone',
-        'province',
-        'canton',
-        'district',
-        'neighborhood',
+        'province_id',
+        'canton_id',
+        'district_id',
+        'neighborhood_id',
         'address',
         'is_active',
     ];
@@ -26,6 +27,26 @@ class Client extends Model
     protected $casts = [
         'is_active' => 'boolean',
     ];
+
+    public function province(): BelongsTo
+    {
+        return $this->belongsTo(Province::class);
+    }
+
+    public function canton(): BelongsTo
+    {
+        return $this->belongsTo(Canton::class);
+    }
+
+    public function district(): BelongsTo
+    {
+        return $this->belongsTo(District::class);
+    }
+
+    public function neighborhood(): BelongsTo
+    {
+        return $this->belongsTo(Neighborhood::class);
+    }
 
     protected static function booted(): void
     {
